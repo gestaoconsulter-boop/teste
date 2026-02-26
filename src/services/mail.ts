ed4fc781d7e4fd21b3868b87738ac577f6a531a5
Juan9321 <juanfariasbr@gmail.com>
2026-02-26 13:13:04 -0300

    email update

--- src/services/mail.ts
+++ src/services/mail.ts
@@ -1,11 +1,15 @@
 import nodemailer from "nodemailer";
 
 export const mailer = nodemailer.createTransport({
+  service: "gmail",
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true, // IMPORTANTE
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
