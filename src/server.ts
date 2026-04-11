71e1456b2b794b3cd4c5d2cb065cf3d629479a89
Juan9321 <juanfariasbr@gmail.com>
2026-04-11 13:52:40 -0300

    .

--- src/server.ts
+++ src/server.ts
@@ -6,27 +6,21 @@ import authRoutes from "./routes/auth.routes";
 import adminRoutes from "./routes/admin.routes";
 import clienteRoutes from "./routes/cliente.routes";
 import consultorRoutes from "./routes/consultor.routes";
-import { checkPayments } from "./jobs/payment.job";
-import healthRoutes from "./routes/health.routes";
 import userRoutes from "./routes/user.routes";
+import healthRoutes from "./routes/health.routes";
+
+import { checkPayments } from "./jobs/payment.job";
 
 checkPayments();
 
 const app = express();
 
-/* =========================
-   CONFIGURAÇÕES GERAIS
-========================= */
-
+/* CONFIG */
 app.use(cors());
-
-app.use(express.json({ limit: "10mb" }));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-/* =========================
-   ROTAS
-========================= */
-
+/* ROTAS */
 app.use("/auth", authRoutes);
 app.use("/admin", adminRoutes);
 app.use("/clientes", clienteRoutes);
@@ -34,10 +28,7 @@ app.use("/consultores", consultorRoutes);
 app.use("/users", userRoutes);
 app.use(healthRoutes);
 
-/* =========================
-   PORTA DINÂMICA (RENDER)
-========================= */
-
+/* PORT */
 const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => {
