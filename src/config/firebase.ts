f01f669be614aed8d65088f475a360f98266cb80
Juan9321 <juanfariasbr@gmail.com>
2026-04-11 13:29:25 -0300

    fix img

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,7 +3,7 @@ import serviceAccount from "../../firebase-service-account.json";
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as any),
-  storageBucket: "gs://teste-741d5.firebasestorage.app",
+  storageBucket: "teste-741d5.appspot.com",
 });
 
 export const bucket = admin.storage().bucket();
